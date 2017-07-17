34459ec6ed64dccc8431c4bc61476d4f068565ae
brad morgan <bkmorgan3@gmail.com>
2017-07-17 12:08:45 -0700

    added search bar

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+console.log("app.js is loaded");
+
+const movieInput = $('#movie-search');
+const searchButton = $('#search-button');
+const results = $('#results');
+
+searchButton.on('click', function(){
+  console.log('clicked')
+})
