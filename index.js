4d256b2b83a5f7bfcbc51d67b27daa5e463f8d69
brad morgan <bkmorgan3@gmail.com>
2017-07-18 10:18:05 -0700

    added styling and update routes

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const
   mongoose = require('mongoose'),
   dotenv = require('dotenv').load(),
   morgan = require('morgan'),
+  methodOverride = require('method-override'),
   passport = require('passport'),
   LocalStrategy = require('passport-local'),
   passportLocalMongoose = require('passport-local-mongoose'),
+  fs = require('fs'),
+  multer = require('multer'),
   request = require('request'),
-  User = require('./models/user')
+  User = require('./models/user'),
+  Post = require('./models/post')
 const
   PORT = 3000,
   app = express()
@@ -41,9 +45,10 @@ passport.deserializeUser(User.deserializeUser());
 //  make the current user available globally
 app.use(function (req, res, next) {
    res.locals.currentUser = req.user;
+   console.log(req.user)
    next()
  });
-console.log(process.env.MDB_API_KEY)
+// console.log(process.env.MDB_API_KEY)
 
  // API Search===============
 
@@ -54,16 +59,72 @@ app.get('/search/:searchTerm', (req, res) => {
   var apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MDB_API_KEY}&language=en-US&query=${searchTerm}&page=1&include_adult=false`
 
   request.get(apiUrl, (err, response, body) => {
-    console.log(body)
+    console.log('body=',body)
     res.json(JSON.parse(body))
   });
 });
 
 //  ROUTES=========================
-//  route to home - all posts.
+  // greeting page
 app.get('/',function(req,res){
-  res.render('home');
+    res.render('home');
 });
+
+//  route to home - all posts.
+app.get('/movies', (req, res) => {
+  Post.find({}, (err, allPosts) => {
+    if(err){
+      console.log(err)
+    } else {
+      res.render('movies', {posts: allPosts})
+    }
+  });
+});
+
+app.post('/movies', isLoggedIn, (req, res) =>{
+  var title = req.body.title
+  var year = req.body.year
+  var image = req.body.image
+  var plot = req.body.plot
+  var newPost = {title:title, year:year, image: image, plot: plot}
+  Post.create(newPost, function(err, newPosting){
+    if(err){
+      console.log(err)
+    } else {
+      res.redirect('/movies')
+    }
+  });
+});
+
+app.get('/movies/new', isLoggedIn, (req, res) => {
+  res.render('new')
+});
+
+app.get('/movies/:id', (req, res) => {
+  Post.findById(req.params.id, (err, foundPost) => {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log(foundPost)
+      res.render('show', {post: foundPost});
+    }
+  });
+});
+
+app.get('/movies/:id/edit', (req,res) => {
+  Post.findById(req.params.id, (err, foundPost) => {
+    if(err){
+      console.log(err)
+    } else {
+      res.render('edit', {post: foundPost});
+    }
+  });
+});
+
+app.put('/movies/:id', (req, res) => {
+  res.send('update route')
+});
+
 
 
 // use to prove auth works.
